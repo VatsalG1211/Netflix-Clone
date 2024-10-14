f4b2bb2f68b16669bdfa0431fd389ed4c13dd568
Vatsal Goswami <vatsalgoswami1211@gmail.com>
2024-10-15 00:26:50 +0530

    made changes

--- netflixclone/urls.py
+++ netflixclone/urls.py
@@ -28,11 +28,3 @@ urlpatterns = [
     path('profile/account/delete-profile/',profile_account_delete,name="profile-account-delete"),
 ]
 
-
-# if settings.DEBUG:
-#     urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
-# else:
-# urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
-# urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
-
-# urlpatterns+=staticfiles_urlpatterns()
